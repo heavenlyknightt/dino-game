da6d21f08502f6a563429f730922ece6ce2f68e2
natanaelmartins <70239432+natanaelmartins@users.noreply.github.com>
2022-07-11 16:28:25 -0300

    Updated the collision message

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
+const errorDiv = document.getElementById("gameover");
 
 function jump() {
   if (dino.classList != "jump") {
@@ -11,6 +12,7 @@ function jump() {
   }
 }
 
+
 let isAlive = setInterval(function () {
 
   let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
@@ -19,11 +21,20 @@ let isAlive = setInterval(function () {
   /* Código para detectar colisão */
 
   if (cactusleft < 50 && cactusleft > 0 && dinoTop >= 140) {
-    alert("Game Over!");
+    errorDiv.classList.remove("visibility");
+    cactus.classList.add("pause");
+    dino.classList.add("pause");
+  }
+  else {
+    errorDiv.classList.add("visibility");
   }
 
 }, 10);
 
 document.addEventListener("keydown", function (event) {
+  jump();
+});
+
+document.addEventListener("touchstart", function (event) {
   jump();
 });
